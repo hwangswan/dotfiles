3401f2a8fa61157b5fb718b4e97eb54ac799bcfb
hwangswan <mrthedzam@gmail.com>
2022-02-03 22:25:09 +0700

    Changed theme to passion

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 sudo apt-get update && sudo apt-get upgrade
 
+ZSH_CUSTOM=~/.oh-my-zsh/custom
+
+# Remove installed style
+rm ~/.oh-my-zsh -rf
+rm ~/.zshrc
+
 # Install zsh
 sudo apt-get install zsh -y
 
@@ -14,7 +20,11 @@ ln -s ~/.zshrc ~/.zshenv
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM}/plugins/zsh-autosuggestions
+
+# Install passion theme
+curl -fLo ${ZSH_CUSTOM}/themes/passion.zsh-theme --create-dirs \
+  https://raw.githubusercontent.com/ChesterYue/ohmyzsh-theme-passion/master/passion.zsh-theme
 
 # Copy .zshrc
 cp .zshrc ~/.zshrc
